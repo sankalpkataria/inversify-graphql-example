252aed8367c4b12a74f571e563b2f574483ce3f4
sankalp <sankalp@habilelabs.io>
2021-05-31 12:10:52 +0530

    logged error

--- src/bin/apollo.ts
+++ src/bin/apollo.ts
@@ -1,16 +1,23 @@
 import { ApolloServer } from 'apollo-server-express';
 import { constants } from '@/config/constants';
 import depthLimit from 'graphql-depth-limit';
-import { injectable } from 'inversify';
+import { inject, injectable } from 'inversify';
 import { GraphQLSchema } from 'graphql';
 import { Resolvers } from '@/modules/';
 import DIContainer from '@/config/di-container';
 import { buildSchema } from 'type-graphql';
+import { Services } from '@/config/services';
+import { ILoggerService } from '@/services/logger.service';
 
-const {NODE_ENV, ENVIRONMENTS, GRAPHQL_DEPTH_LIMIT} = constants;
+const {NODE_ENV, ENVIRONMENTS, GRAPHQL_DEPTH_LIMIT, LOG_LEVELS, ERRORS } = constants;
 
 @injectable()
 export class Apollo {
+  constructor(
+    @inject(Services.LoggerService) 
+    private readonly logger: ILoggerService,
+  ) {}
+
   private async createSchema (): Promise<GraphQLSchema> {
     return buildSchema({
       resolvers: Resolvers,
@@ -22,6 +29,7 @@ export class Apollo {
 
   private formatError(error: Error) {
     //  Format errors here
+    console.error(error, 'error');
     return error;
   }
 
